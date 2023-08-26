b00dff277f4671006365dbeff69d339bc1925fba
yash <yashvaghasiya440@gmail.com>
2023-08-26 07:52:28 -0700

    [MOD]Change the logic of total

--- Paper_2/Paper_2/models/project_project.py
+++ Paper_2/Paper_2/models/project_project.py
@@ -5,15 +5,17 @@ class Project(models.Model):
     _inherit = "project.project"
 
     hours_total = fields.Float(string="Hours Project")
-    hours_total_planned = fields.Float(string="Hours Total Planned", compute="_compute_hours_total_plannned", store=1)  # Computed Field ( hours total - sum (One2many
-    # field))
+    hours_total_planned = fields.Float(string="Hours Total Planned", compute="_compute_hours_total_plannned", store=1)
     planning_line_ids = fields.One2many('project.planning.line', 'project_id', string="Planning Hours")
-    # name = fields.Char()
 
     @api.depends('hours_total','planning_line_ids.hours_invested')
     def _compute_hours_total_plannned(self):
-        for rec in self:
-            rec.hours_total_planned = self.hours_total - sum(rec.planning_line_ids.mapped('hours_invested'))
-            print("\n\n\n\n\n\n\n\nHello Project.Project Module",sum(rec.planning_line_ids.mapped('hours_invested')))
+        total = 0
+        for rec in self.planning_line_ids:
+            total += rec.hours_invested
+
+        self.hours_total_planned = self.hours_total - total
+        # rec.hours_total_planned = self.hours_total - sum(rec.planning_line_ids.mapped('hours_invested'))
+
 
 
